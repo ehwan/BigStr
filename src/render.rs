676772a3456bf2c369f50430fe44e81c50f4e7ae
Taehwan Kim <ehwank98@gmail.com>
2024-06-25 21:36:48 +0900

    remove invalid printing

--- src/render.rs
+++ src/render.rs
@@ -16,14 +16,14 @@ pub fn render_char<'a>(font: &Font<'a>, ch: char, line_height: usize) -> Vec<f32
     let image_width = hmetrics.advance_width.ceil() as usize;
     let image_height = (vmetrics.ascent - vmetrics.descent).ceil() as usize;
 
-    println!(
-        "image_width: {}, image_height: {}",
-        image_width, image_height
-    );
+    // println!(
+    //     "image_width: {}, image_height: {}",
+    //     image_width, image_height
+    // );
 
     let glyph = glyph.positioned(offset);
     let bb = glyph.pixel_bounding_box();
-    println!("bb: {:?}", bb);
+    // println!("bb: {:?}", bb);
     let mut ret = Vec::new();
     ret.resize(image_width * image_height, 0.0f32);
     if let Some(bb) = bb {
